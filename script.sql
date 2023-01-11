d53ee053ddbe9ba1b5392bccbeeb55dee34ff829
kk-2503 <alu0101109046@ull.edu.es>
2023-01-11 21:33:27 +0000

    some more tables

--- script.sql
+++ script.sql
@@ -1,7 +1,20 @@
-DROP TABLE IF EXISTS ciudad;
-DROP SEQUENCE IF EXISTS public.ciudad_id_ciudad_seq;
+DROP TABLE IF EXISTS compra;
+DROP SEQUENCE IF EXISTS public.compra_id_compra_seq;
+DROP TABLE IF EXISTS tienda_tiene_prod;
 DROP TABLE IF EXISTS tienda;
 DROP SEQUENCE IF EXISTS public.tienda_id_tienda_seq;
+DROP TABLE IF EXISTS ciudad;
+DROP SEQUENCE IF EXISTS public.ciudad_id_ciudad_seq;
+DROP TABLE IF EXISTS marca;
+DROP SEQUENCE IF EXISTS public.marca_id_marca_seq;
+DROP TABLE IF EXISTS producto;
+DROP SEQUENCE IF EXISTS public.producto_id_producto_seq;
+DROP TABLE IF EXISTS dependiente;
+DROP SEQUENCE IF EXISTS public.dependiente_id_empleado_seq;
+DROP TABLE IF EXISTS encargado_tienda;
+DROP SEQUENCE IF EXISTS public.encargado_tienda_id_enc_seq;
+DROP TABLE IF EXISTS departamento;
+DROP SEQUENCE IF EXISTS public.departamento_id_dep_seq;
 
 CREATE SEQUENCE public.ciudad_id_ciudad_seq
     START WITH 1
@@ -24,6 +37,35 @@ VALUES(DEFAULT, 'canarias', 'españa');
 INSERT INTO ciudad
 VALUES(DEFAULT, 'madrid', 'españa');
 
+SELECT *
+FROM ciudad;
+
+---------------------------------------------TABLA MARCA-----------------------------------------
+
+CREATE SEQUENCE public.marca_id_marca_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE marca (
+  id_marca integer PRIMARY KEY DEFAULT nextval('public.marca_id_marca_seq'::regclass) NOT NULL,
+  nom_marca VARCHAR(150) NOT NULL,
+  prod_principal VARCHAR(200) NOT NULL,
+  CONSTRAINT unique_vals_marca
+  UNIQUE (nom_marca)
+);
+
+INSERT INTO marca
+VALUES(DEFAULT, 'Zara', 'Prendas');
+
+INSERT INTO marca
+VALUES(DEFAULT, 'Zara Home', 'Decoracion de Casa');
+
+SELECT *
+FROM marca;
+
 -------------------------------------------TABLA TIENDA-----------------------------
 
 CREATE SEQUENCE public.tienda_id_tienda_seq
@@ -38,17 +80,197 @@ CREATE TABLE tienda (
 	nombre VARCHAR(200) NOT NULL,
 	direccion VARCHAR(300) NOT NULL,
   id_ciudad INTEGER NOT NULL,
+  id_marca INTEGER NOT NULL,
 	CONSTRAINT fk_ciudad
 	FOREIGN KEY(id_ciudad)
 	REFERENCES ciudad(id_ciudad)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE,
+  CONSTRAINT fk_marca
+	FOREIGN KEY(id_marca)
+	REFERENCES marca(id_marca),
   CONSTRAINT unique_vals_tienda
   UNIQUE (nombre)
 );
 
 INSERT INTO tienda
-VALUES(DEFAULT, 'zara-la laguna', 'abc', 1);
+VALUES(DEFAULT, 'zara-la laguna', 'abc', 1, 1);
 
 INSERT INTO tienda
-VALUES(DEFAULT, 'zara-gran via', 'def', 2);
+VALUES(DEFAULT, 'zara home-gran via', 'def', 2, 2);
+
+SELECT *
+FROM tienda;
+
+---------------------------------------------TABLA COMPRA-------------------------------------------------
+
+CREATE SEQUENCE public.compra_id_compra_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE compra (
+  id_compra integer PRIMARY KEY DEFAULT nextval('public.compra_id_compra_seq'::regclass) NOT NULL,
+  precio_total NUMERIC(8,2) NOT NULL,
+  fecha DATE NOT NULL,
+  id_tienda INTEGER NOT NULL,
+  CONSTRAINT fk_tienda
+	FOREIGN KEY(id_tienda)
+	REFERENCES tienda(id_tienda)
+	ON DELETE RESTRICT
+	ON UPDATE CASCADE
+);
+
+INSERT INTO compra
+VALUES(DEFAULT, 25.30, '2023-01-05', 1);
+
+INSERT INTO compra
+VALUES(DEFAULT, 10, '2023-01-06', 2);
+
+INSERT INTO compra
+VALUES(DEFAULT, 10, '2023-01-06', 1);
+
+SELECT *
+FROM compra;
+
+----------------------------------------------TABLA PRODUCTO------------------------------------------------
+
+CREATE SEQUENCE public.producto_id_producto_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE producto (
+  id_producto integer PRIMARY KEY DEFAULT nextval('public.producto_id_producto_seq'::regclass) NOT NULL,
+  nombre_prod VARCHAR(300) NOT NULL,
+  precio_prod NUMERIC(8,2) NOT NULL
+);
+
+INSERT INTO producto
+VALUES(DEFAULT, 'Camisa hombre', 6.99);
+
+INSERT INTO producto
+VALUES(DEFAULT, 'Tacones rojo', 12);
+
+SELECT *
+FROM producto;
+
+---------------------------------------------TABLA TIENDA-TIENE-PRODUCTO----------------------------------
+
+CREATE TABLE tienda_tiene_prod (
+	id_tienda INTEGER NOT NULL,
+	id_producto INTEGER NOT NULL,
+	cantidad INT NOT NULL,
+	CONSTRAINT fk_tienda
+	FOREIGN KEY(id_tienda)
+	REFERENCES tienda(id_tienda),
+	CONSTRAINT fk_producto
+	FOREIGN KEY(id_producto)
+	REFERENCES producto(id_producto)
+);
+
+INSERT INTO tienda_tiene_prod
+VALUES(1, 1, 73);
+
+INSERT INTO tienda_tiene_prod
+VALUES(1, 2, 21);
+
+SELECT id_producto, id_tienda, cantidad, nombre, nombre_prod
+FROM tienda_tiene_prod NATURAL JOIN tienda NATURAL JOIN producto;
+
+-------------------------------------------------TABLA DEPENDIENTE----------------------------------------
+
+CREATE SEQUENCE public.dependiente_id_empleado_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE dependiente (
+  id_empleado integer PRIMARY KEY DEFAULT nextval('public.dependiente_id_empleado_seq'::regclass) NOT NULL,
+  nombre_dep VARCHAR(100) NOT NULL,
+  apellido1_dep VARCHAR(100) NOT NULL,
+  apellido2_dep VARCHAR(100),
+  email VARCHAR(200) NOT NULL,
+  direccion VARCHAR(300) NOT NULL,
+  fecha_ini DATE NOT NULL,
+  telefono VARCHAR(9) NOT NULL,
+  CONSTRAINT unique_vals_dep
+  UNIQUE (nombre_dep, apellido1_dep, direccion)
+);
+
+INSERT INTO dependiente
+VALUES(DEFAULT, 'Karina', 'Kalwani', NULL, 'alu0101109046@ull.edu.es', 'abc', '2023-01-04', '123456789');
+
+INSERT INTO dependiente
+VALUES(DEFAULT, 'Noelia', 'Ibañez', 'Silvestre', 'alu0101225555@ull.edu.es', 'abc', '2023-01-04', '123456789');
+
+SELECT *
+FROM dependiente;
+
+-------------------------------------------------TABLA ENCARGADO TIENDA----------------------------------------
+
+CREATE SEQUENCE public.encargado_tienda_id_enc_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE encargado_tienda (
+  id_enc integer PRIMARY KEY DEFAULT nextval('public.encargado_tienda_id_enc_seq'::regclass) NOT NULL,
+  nombre_enc VARCHAR(100) NOT NULL,
+  apellido1_enc VARCHAR(100) NOT NULL,
+  apellido2_enc VARCHAR(100),
+  email VARCHAR(200) NOT NULL,
+  direccion VARCHAR(300) NOT NULL,
+  fecha_ini DATE NOT NULL,
+  telefono VARCHAR(9) NOT NULL,
+  CONSTRAINT unique_vals_enc
+  UNIQUE (nombre_enc, apellido1_enc, direccion)
+);
+
+INSERT INTO encargado_tienda
+VALUES(DEFAULT, 'Karina', 'Kalwani', NULL, 'alu0101109046@ull.edu.es', 'abc', '2023-01-04', '123456789');
+
+INSERT INTO encargado_tienda
+VALUES(DEFAULT, 'Noelia', 'Ibañez', 'Silvestre', 'alu0101225555@ull.edu.es', 'abc', '2023-01-04', '123456789');
+
+SELECT *
+FROM encargado_tienda;
+
+----------------------------------------------TABLA DEPARTAMENTO--------------------------------------------
+
+CREATE SEQUENCE public.departamento_id_dep_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE departamento (
+  id_dep integer PRIMARY KEY DEFAULT nextval('public.departamento_id_dep_seq'::regclass) NOT NULL,
+  nombre_dep VARCHAR(200) NOT NULL,
+  puesto VARCHAR(200) NOT NULL,
+  nombre_emp VARCHAR(100) NOT NULL,
+  apellido1_emp VARCHAR(100) NOT NULL,
+  apellido2_emp VARCHAR(100),
+  email VARCHAR(200) NOT NULL,
+  direccion VARCHAR(300) NOT NULL,
+  fecha_ini DATE NOT NULL,
+  telefono VARCHAR(9) NOT NULL
+);
+
+INSERT INTO departamento
+VALUES(DEFAULT, 'Marketing', 'abc','Karina', 'Kalwani', NULL, 'alu0101109046@ull.edu.es', 'abc', '2023-01-04', '123456789');
+
+INSERT INTO departamento
+VALUES(DEFAULT, 'Marketing', 'def', 'Noelia', 'Ibañez', 'Silvestre', 'alu0101225555@ull.edu.es', 'abc', '2023-01-04', '123456789');
+
+SELECT *
+FROM departamento;
